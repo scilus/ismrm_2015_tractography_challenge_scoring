f867afd6b0d2a4c1ae0298f4675ad3903cf8d29a
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2017-05-13 13:31:48 -0400

    RF: PEP8 and some comments

--- scripts/score_tractogram.py
+++ scripts/score_tractogram.py
@@ -3,13 +3,11 @@
 from __future__ import division
 
 import argparse
-import os
-import pickle
 import logging
+import os
 
-# TODO change this is ugly
-from challenge_scoring.metrics.scoring import score_submission
 from challenge_scoring.io.results import save_results
+from challenge_scoring.metrics.scoring import score_submission
 from challenge_scoring.utils.attributes import get_attribs_for_file,\
                                                load_attribs
 from challenge_scoring.utils.filenames import mkdir
@@ -17,32 +15,31 @@ from challenge_scoring.utils.filenames import mkdir
 
 DESCRIPTION = """
     Score a submission for the ISMRM 2015 tractography challenge.
-    
-    This is based on the ISMRM 2015 tractography challenge, see 
+
+    This is based on the ISMRM 2015 tractography challenge, see
     http://www.tractometer.org/ismrm_2015_challenge/
-    
-    This script scores a submission following the method presented in 
+
+    This script scores a submission following the method presented in
     https://doi.org/10.1101/084137
-    
-    This method differs from the classical Tractometer approach 
-    (https://doi.org/10.1016/j.media.2013.03.009). Instead of only using 
-    masks to define the ground truth and classify streamlines in the 
+
+    This method differs from the classical Tractometer approach
+    (https://doi.org/10.1016/j.media.2013.03.009). Instead of only using
+    masks to define the ground truth and classify streamlines in the
     submission, bundles are extracted using a bundle recognition technique.
-    
-    More details are provided in the documentation here: 
+
+    More details are provided in the documentation here:
     https://github.com/scilus/ismrm_2015_tractography_challenge_scoring
-    
+
     The algorithm has 6 main steps:
         1: extract all streamlines that are valid, which are classified as
            Valid Connections (VC) making up Valid Bundles (VB).
         2: remove streamlines shorter than an threshold based on the GT dataset
         3: cluster the remaining streamlines
         4: remove singletons
-        5: assign each cluster to the closest ROIs pair. Those make up the 
+        5: assign each cluster to the closest ROIs pair. Those make up the
            Invalid Connections (IC), grouped as Invalid Bundles (IB).
-        6: streamlines that are neither in VC nor IC are classified as 
+        6: streamlines that are neither in VC nor IC are classified as
            No Connection (NC).
-
 """
 
 
@@ -54,22 +51,24 @@ def buildArgsParser():
                    metavar='TRACTS', type=str, help='Tractogram file')
 
     # TODO where do we host this?
-    p.add_argument('base_dir',   action='store',
-                   metavar='BASE_DIR',  type=str,
-                   help='base directory for scoring data')
+    p.add_argument('base_dir', action='store',
+                   metavar='BASE_DIR', type=str,
+                   help='base directory for scoring data.\n'
+                        'See www.tractometer.org/downloads/downloads/'
+                        'scoring_data_tractography_challenge.tar.gz')
 
     p.add_argument('metadata_file', action='store',
-                   metavar='SUBMISSIONS_ATTRIBUTES', type=str,
-                   help='attributes file of the submissions. ' +\
-                        'Needs to contain the orientation.\n' +\
-                        'Normally, use metadata/ismrm_challenge_2015/' +\
-                        'anon_submissions_attributes.json.\n' +\
-                        'Can be computed with ' +\
+                   metavar='TRACT_ATTRIBUTES', type=str,
+                   help='attributes file of the submission.'
+                        'Needs to contain the orientation.\n'
+                        'Normally, use metadata/ismrm_challenge_2015/' +
+                        'anon_submissions_attributes.json.\n' +
+                        'Can be computed with ' +
                         'ismrm_compute_submissions_attributes.py.')
 
     p.add_argument('basic_bundles_attribs', action='store',
                    metavar='GT_ATTRIBUTES', type=str,
-                   help='attributes of the basic bundles. ' +\
+                   help='attributes of the basic bundles. ' +
                         'Same format as SUBMISSIONS_ATTRIBUTES')
 
     p.add_argument('out_dir',    action='store',
@@ -88,7 +87,6 @@ def buildArgsParser():
     p.add_argument('--save_vb', action='store_true',
                    help='save VB independently.')
 
-    #Other
     p.add_argument('-f', dest='is_forcing', action='store_true',
                    required=False, help='overwrite output files')
     p.add_argument('-v', dest='is_verbose', action='store_true',
@@ -110,7 +108,6 @@ def main():
     isVerbose = args.is_verbose
 
     if isVerbose:
-        #helper.VERBOSE = True
         # TODO check level to send to scoring
         logging.basicConfig(level=logging.DEBUG)
 
@@ -129,7 +126,6 @@ def main():
     if out_dir is not None:
         out_dir = mkdir(out_dir + "/").replace("//", "/")
 
-
     scores_dir = mkdir(os.path.join(out_dir, "scores"))
     # TODO remove all pkl mentions
     scores_filename = scores_dir + tractogram.split('/')[-1][:-4] + ".pkl"
@@ -142,7 +138,8 @@ def main():
             return
 
     # TODO support just giving the orientation attribute
-    tracts_attribs = get_attribs_for_file(attribs_file, os.path.basename(tractogram))
+    tracts_attribs = get_attribs_for_file(attribs_file,
+                                          os.path.basename(tractogram))
     basic_bundles_attribs = load_attribs(args.basic_bundles_attribs)
 
     # TODO remove files in out_dir segmented
@@ -163,12 +160,8 @@ def main():
                               segments_dir, base_name, isVerbose)
 
     if scores is not None:
-        # TODO temp
-        pickle.dump(scores, open(scores_filename, 'wb'))
         save_results(scores_filename[:-4] + '.json', scores)
 
-    if isVerbose:
-        print(scores)
 
 if __name__ == "__main__":
     main()
